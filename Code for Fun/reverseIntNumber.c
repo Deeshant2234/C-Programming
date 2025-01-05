57b4498ca6a7152a91a2f502c9ab2c5dbb0560f6
Deeshant2234 <dishantsharma203058@gmail.com>
2025-01-05 17:35:38 +0530

    Code updated

--- Code for Fun/reverseIntNumber.c
+++ Code for Fun/reverseIntNumber.c
@@ -7,7 +7,7 @@ int rev_i(int i)
     while (i > 0) { 
         digit = i % 10;
         /* Multiply by 10 to shift left and add last digit */
-        rev_num = rev_num * 10 + digit; rev_num * 10 + i % 10;  
+        rev_num = rev_num * 10 + digit; 
         i = i / 10; 
     } 
     return rev_num; 
